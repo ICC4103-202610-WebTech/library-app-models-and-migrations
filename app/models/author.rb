ab1a57e3e37c1d97774b372ab48e016d9fd14661
Andrés Howard <ahowardm@gmail.com>
2026-04-21 09:40:31 -0400

    validaitons and callbacks

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,13 @@
 class Author < ApplicationRecord
   has_many :books
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  before_validation :clean_name
+
+  def clean_name
+    self.first_name = first_name.strip.titleize
+    self.last_name = last_name.strip.titleize
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :author
+
+  validates :title, presence: true
+
+  before_validation :clean_title
+
+  def clean_title
+    self.title = title.strip.titleize
+  end
 end
